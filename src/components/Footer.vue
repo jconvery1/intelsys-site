3bf841c875d17f530683b30e6036269eeefff818
jconvery1 <jconvery14@protonmail.com>
2024-02-04 13:33:50 +0000

    improved mobile footer layout

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,24 +1,18 @@
 <template>
 <footer class="bg-black">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-10">
-        <div class="md:flex md:justify-between">
+        <div class="flex justify-between">
           <div class="mb-6 md:mb-0">
               <a href="https://intelsys.uk/" class="flex items-center">
                   <img src="../assets/images/intelsys_text.jpg" class="h-6 me-3" alt="Intelsys Ltd." />
               </a>
           </div>
-          <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-2">
+          <div class="grid gap-8">
             <div>
                 <h2 class="mb-6 text-sm font-semibold uppercase text-white">Follow us</h2>
                 <ul class="text-gray-400 font-medium">
                     <li class="mb-4">
                         <a href="#" class="hover:underline ">LinkedIn</a>
-                    </li>
-                    <li class="mb-4">
-                        <a href="#" class="hover:underline">TikTok</a>
-                    </li>
-                    <li>
-                        <a href="#" class="hover:underline">Facebook</a>
                     </li>
                 </ul>
             </div>
